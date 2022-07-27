b9b924c0de000317421ce3aa31d3ca40a3810546
Valery Shcherbinin <fundroin@mail.ru>
2022-07-27 22:18:42 +0500

    4 добавляет новый компонент списка товаров

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="products-list">
+    <product-element v-for="product in products" :key="product.id" :product="product" class="products__item" />
+  </div>
+</template>
+
+<script>
+  import productElement from "@/components/ProductElement";
+  export default {
+    name: "ProductsList",
+    components: { productElement },
+    props: {
+      products: {
+        type: Array,
+        default: () => []
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .products-list {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 332px);
+    grid-gap: 16px;
+  }
+</style>
