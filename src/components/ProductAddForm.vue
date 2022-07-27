16d51bdc37bcb8c24da8f9853b6dc66b1b18211e
Valery Shcherbinin <fundroin@mail.ru>
2022-07-27 20:59:36 +0500

    2 добавляет и подключает форму для добавления товаров на стартовую страницу

---
+++ src/components/ProductAddForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <form class="add-form">
+    <div class="add-form__input">
+      <v-label :required="true" for="name">Наименование товара</v-label>
+      <v-input id="name" placeholder="Введите наименование товара" />
+    </div>
+
+    <div class="add-form__input">
+      <v-label for="description">Описание товара</v-label>
+      <v-textarea id="description" placeholder="Введите описание товара" rows="5"></v-textarea>
+    </div>
+
+    <div class="add-form__input">
+      <v-label for="link">Ссылка на изображение товара</v-label>
+      <v-input id="link" placeholder="Введите ссылку" />
+    </div>
+
+    <div class="add-form__input">
+      <v-label for="price">Цена товара</v-label>
+      <v-input id="price" placeholder="Введите цену" />
+    </div>
+
+    <v-button type="submit">Добавить товар</v-button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: "ProductAddForm"
+  }
+</script>
+
+<style scoped lang="scss">
+  .add-form {
+    padding: 24px;
+    background: #FFFEFB;
+    box-shadow: 0 20px 30px rgba(0, 0, 0, 0.04), 0 6px 10px rgba(0, 0, 0, 0.02);
+    border-radius: 4px;
+    display: flex;
+    flex-direction: column;
+    row-gap: 16px;
+
+    &__input {
+      display: flex;
+      flex-direction: column;
+      row-gap: 6px;
+    }
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,32 @@
 <template>
+  <div class="homepage">
+    <div class="container">
+      <div class="homepage__header">
+        <h1 class="homepage__title">Добавление товара</h1>
+      </div>
+      <product-add-form class="homepage__form"/>
+    </div>
+  </div>
 </template>
 
 <script>
-
-export default {
-  name: 'HomeView',
-  components: {}
-}
+  import productAddForm from "@/components/ProductAddForm";
+  export default {
+    name: 'HomeView',
+    components: { productAddForm },
+  }
 </script>
+
+<style scoped lang="scss">
+  .homepage {
+    padding: 32px;
+
+    &__header {
+      margin-bottom: 8px;
+    }
+
+    &__form {
+      max-width: 332px;
+    }
+  }
+</style>
