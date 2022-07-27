3e13c140e93c2e44d4729f16178adc4957f62cb2
Valery Shcherbinin <fundroin@mail.ru>
2022-07-27 20:56:32 +0500

    2 добавляет и регистрирует UI компоненты

---
+++ src/components/UI/v-label.vue
@@ -0,0 +1,39 @@
+<template>
+  <label class="label" :class="required ? 'label--required' : ''">
+    <slot></slot>
+  </label>
+</template>
+
+<script>
+  export default {
+    name: "v-label",
+    props: {
+      required: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .label {
+    font-weight: 400;
+    font-size: 12px;
+    letter-spacing: -0.02em;
+    color: #49485E;
+
+    &--required {
+      position: relative;
+
+      &::after {
+        position: absolute;
+        content: '';
+        width: 4px;
+        height: 4px;
+        background: #FF8484;
+        border-radius: 100%;
+      }
+    }
+  }
+</style>
