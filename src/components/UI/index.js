2ff263cfbca23fa3b93eff86593f2df5e813ffca
Valery Shcherbinin <fundroin@mail.ru>
2022-07-28 22:26:45 +0500

    7 добавляет валидацию для формы добавления товара

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -3,7 +3,8 @@ import VButton from "@/components/UI/v-button";
 import VLabel from "@/components/UI/v-label";
 import VTextarea from "@/components/UI/v-textarea";
 import VButtonDelete from "@/components/UI/v-button-delete";
+import VErrorMessages from "@/components/UI/v-error-messages";
 
 export default [
-    VInput, VButton, VLabel, VTextarea, VButtonDelete
+    VInput, VButton, VLabel, VTextarea, VButtonDelete, VErrorMessages
 ];
---
+++ src/components/UI/v-error-messages.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="error" class="errors">{{ error }}</div>
+</template>
+
+<script>
+export default {
+  name: "v-error-messages",
+  props: {
+    error: [ String, Boolean ]
+  }
+}
+</script>
+
+<style scoped>
+  .errors {
+    font-weight: 400;
+    font-size: 12px;
+    letter-spacing: -0.02em;
+    color: #FF8484;
+  }
+</style>
