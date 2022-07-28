bf0a8f6010725e2d779af045e6b438c889627a48
Valery Shcherbinin <fundroin@mail.ru>
2022-07-29 00:05:30 +0500

    8 добавляет select и сортировку списка

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -4,7 +4,8 @@ import VLabel from "@/components/UI/v-label";
 import VTextarea from "@/components/UI/v-textarea";
 import VButtonDelete from "@/components/UI/v-button-delete";
 import VErrorMessages from "@/components/UI/v-error-messages";
+import VSelect from "@/components/UI/v-select";
 
 export default [
-    VInput, VButton, VLabel, VTextarea, VButtonDelete, VErrorMessages
+    VInput, VButton, VLabel, VTextarea, VButtonDelete, VErrorMessages, VSelect
 ];
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,11 @@
     <div class="container">
       <div class="homepage__header">
         <h1 class="homepage__title">Добавление товара</h1>
+        <v-select :options="sortOptions" :model-value="selectedSort" @update:model-value="setSelectedSort" class="homepage__select"></v-select>
       </div>
       <div class="homepage__body">
         <product-add-form class="homepage__form"/>
-        <products-list :products="products" class="homepage__products"/>
+        <products-list :products="sortedProducts" class="homepage__products"/>
       </div>
     </div>
   </div>
@@ -15,15 +16,25 @@
 <script>
   import productAddForm from "@/components/ProductAddForm";
   import productsList from "@/components/ProductsList";
-  import { mapState } from 'vuex';
+  import { mapState, mapMutations, mapGetters } from 'vuex';
 
   export default {
     name: 'HomeView',
     components: { productsList, productAddForm },
+    methods: {
+      ...mapMutations({
+        setSelectedSort: 'SET_SELECTED_SORT'
+      }),
+    },
     computed: {
       ...mapState({
         products: state => state.products,
+        sortOptions: state => state.sortOptions,
+        selectedSort: state => state.selectedSort,
       }),
+      ...mapGetters({
+        sortedProducts: 'SORTED_PRODUCTS',
+      })
     },
   }
 </script>
@@ -33,7 +44,19 @@
     padding: 32px 0;
 
     &__header {
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      gap: 20px;
       margin-bottom: 8px;
+    }
+
+    &__select {
+      align-self: flex-end;
+
+      @media screen and (max-width: 740px) {
+        width: 100%;
+      }
     }
 
     &__form {
@@ -49,6 +72,10 @@
       display: flex;
       width: 100%;
       gap: 16px;
+
+      @media screen and (max-width: 1091px) {
+        flex-direction: column;
+      }
 
       @media screen and (max-width: 740px) {
         justify-content: center;
